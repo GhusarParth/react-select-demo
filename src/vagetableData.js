563bfb32750204b08fee6eb9acd7db0be8c3f82b
GhusarParth <93975400+GhusarParth@users.noreply.github.com>
2021-11-13 10:17:33 +0530

    vagitable data

---
+++ src/vagetableData.js
@@ -0,0 +1,13 @@
+const vegitables=[
+    {lable:"Tomato",value:"Tomato"},
+    {lable:"Cauliflower", value:"Cauliflower"},
+    {lable:"Carrot", value:"Carrot"},
+    {lable:"Capsicum",value:"Capsicum"},
+    {lable:"Cabbage",value:"Cabbage"},
+    {lable:"Potato",value:"Potato"},
+    {lable:"Lady-Finger",value:"Lady-Finger"},
+    {lable:"Onion",value:"Onion"},
+    {lable:"Sweet-Potato",value:"Sweet-Potato"},
+    {lable:"Qucumber",value:"Qucumber"}
+]
+
